18ec104bb141edba5f3d38985705066e988a564d
bob walker <bob@dxw.com>
2018-12-21 13:49:43 +0000

    Rework error message output
    - markdownlint adds a useful link but in a way we don't like so remove it
    - the error message is now more human readable

--- test.sh
+++ test.sh
@@ -9,10 +9,14 @@ then
   exit 0
 else
   bundle exec mdl -g . | \
-    awk -F: '/MD/ {print $3}' | \
-    sed -e "s/^ //" -e "s/MD\([0-9]*\) /MD\1 - /" | \
-    tr " " "-" | \
-    tr '[:upper:]' '[:lower:]' | \
-    sed -e "s?^?To fix this see https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#?"
+    # only parse lines which have MD in them
+    grep MD | \
+    # make sure there isnt an extra space and then add a dash after the error code
+    sed -e "s/: /:/" -e "s/MD\([0-9]*\) /MD\1 - /" | \
+    # add the error again but as lower case
+    awk -F: '{print $0 ":" tolower($3)}' | \
+    # replace the spaces with dashes in the lowercase message and then print out
+    # our friendly error message with a url
+    awk -F: '{ gsub(" ","-",$4); print "There is an error in "$1" on line "$2" \""$3"\" see https://github.com/markdownlint/markdownlint/blob/master/docs/RULES.md#"$4" to fix it"}'
   exit 1
 fi
