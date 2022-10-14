995a38e58220180712bea3c172201287a89895f8
George Eaton <george@dxw.com>
2022-10-14 14:14:32 +0100

    Style search results and update excerpt
    We've made the matching search term bold in search results. See the
    example of this style below:
    
    @see https://www.gov.uk/search/all?keywords=energy&order=relevance
    
    The differentiation between bold and regular weight with our colour
    palette is not as strong as in the example above. Therefore, we also
    recoloured the search results to mid-blue. We don't think this looks
    perfect, but we do think it's more legible
    
    This also fetches the surrounding 200 characters to be displayed as an
    excerpt. These are aligned with word boundaries, by moving outward until
    a space is found, then trimming the excerpt to exclude the space
    
    Unfortunately Markdown isn't being rendered here as this is an HTML file
    (Jekyll's Markdown parsing is only applied to `.md` files) so we may
    need to add our own Markdown parsing here, or find another way of using
    Jekyll's built-in one.
    
    Detruncate words in search result excerpts
    
    The list style is removed both from search results and the messages
    shown if there are no results
    
    Co-authored-by: Ynda Jas <yndajas@gmail.com>

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,5 +1,44 @@
 (() => {
-  const displaySearchResults = (results) => {
+  const getExcerpt = (content, searchQuery) => {
+    const queryRegex = new RegExp(searchQuery, "i");
+    const matchIndex = content.search(queryRegex);
+    const queryLength = searchQuery.length;
+    const excerpt = content.slice(
+      getStartIndex(matchIndex, content),
+      getEndIndex(matchIndex, queryLength, content),
+    );
+
+    return excerpt.replace(
+      queryRegex,
+      "<strong class='search-results__matching-keyword'>$&</strong>"
+    );
+  };
+
+  const getStartIndex = (matchIndex, content) => {
+    let startIndex = Math.max(matchIndex - 100, 0);
+
+    while (startIndex > 0 && content[startIndex] !== " ") {
+      startIndex = startIndex - 1;
+    };
+
+    if (content[startIndex] === " ") { startIndex++; };
+
+    return startIndex;
+  };
+
+  const getEndIndex = (matchIndex, queryLength, content) => {
+    matchIndex = Math.max(matchIndex, 100);
+
+    let endIndex = Math.min(matchIndex + queryLength + 100, content.length);
+
+    while (endIndex !== content.length && content[endIndex] !== " ") {
+      endIndex++;
+    };
+
+    return endIndex;
+  };
+
+  const displaySearchResults = (results, searchQuery) => {
     const searchResultsElement = document.getElementById("search-results");
 
     if (results.length) {
@@ -8,13 +47,14 @@
       results.forEach((result) => {
         const item = window.store[result.ref];
         innerHtml +=
-          '<li><a href="' + item.url + '"><h2>' + item.title + "</h2></a>";
-        innerHtml += "<p>" + item.content.substring(0, 150) + "...</p></li>";
+          '<li class="search-results__result"><a href="' + item.url + '"><h2>' + item.title + "</h2></a>";
+        innerHtml +=
+          "<p>..." + getExcerpt(item.content, searchQuery) + "...</p></li>";
       });
 
       searchResultsElement.innerHTML = innerHtml;
     } else {
-      searchResultsElement.innerHTML = "<li>No results found.</li>";
+      searchResultsElement.innerHTML = "<li class='search-results__no-results-message'>No results found.</li>";
     }
   };
 
@@ -44,6 +84,6 @@
     }
 
     const results = index.search(searchQuery);
-    displaySearchResults(results);
+    displaySearchResults(results, searchQuery);
   }
 })();
