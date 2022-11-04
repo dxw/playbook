804fcf5edebb0bf9d7ba7e1fad8e5d281961168e
Ynda Jas <yndajas@gmail.com>
2022-11-04 11:07:57 +0000

    Restructure `search.js` by call order

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,42 +1,16 @@
 (() => {
-  const getExcerpt = (content, query) => {
-    const queryRegex = new RegExp(query, "i");
-    const searchIndex = content.search(queryRegex);
-    const queryLength = query.length;
-    const excerpt = content.slice(
-      getStartIndex(searchIndex, content),
-      getEndIndex(searchIndex, queryLength, content),
-    );
-
-    return excerpt.replace(
-      queryRegex,
-      "<strong class='search-results__matching-keyword'>$&</strong>"
-    );
+  const getQueryVariable = (variable) => {
+    const urlQueryString = window.location.search;
+    const urlParams = new URLSearchParams(urlQueryString);
+    return urlParams.get(variable);
   };
 
-  const getStartIndex = (searchIndex, content) => {
-    let startIndex = searchIndex - 100;
-
-    if (startIndex < 0) { startIndex = 0; };
-
-    while (startIndex > 0 && content[startIndex] !== " ") {
-      startIndex = startIndex - 1;
-    };
-
-    if (content[startIndex] === " ") { startIndex++; };
-
-    return startIndex;
-  };
-
-  const getEndIndex = (searchIndex, queryLength, content) => {
-    let endIndex = searchIndex + queryLength + 100;
-
-    while (endIndex !== content.length && content[endIndex] !== " ") {
-      endIndex++;
-    };
-
-    return endIndex;
-  };
+  const formatContent = (rawContent) => {
+    return rawContent
+      .replace(/([\.\?\!])[\n\s]{2,}/g, "$1 ")
+      .replace(/[\n\s]{2,}/g, ". ")
+      .replace(/\n/, " ");
+  }
 
   const displaySearchResults = (results, store, searchTerm) => {
     const searchResultsElement = document.getElementById("search-results");
@@ -81,20 +55,44 @@
     }
   };
 
-  const getQueryVariable = (variable) => {
-    const urlQueryString = window.location.search;
-    const urlParams = new URLSearchParams(urlQueryString);
-    return urlParams.get(variable);
+  const getExcerpt = (content, query) => {
+    const queryRegex = new RegExp(query, "i");
+    const searchIndex = content.search(queryRegex);
+    const queryLength = query.length;
+    const excerpt = content.slice(
+      getStartIndex(searchIndex, content),
+      getEndIndex(searchIndex, queryLength, content),
+    );
+
+    return excerpt.replace(
+      queryRegex,
+      "<strong class='search-results__matching-keyword'>$&</strong>"
+    );
   };
 
-  const searchTerm = getQueryVariable("query");
+  const getStartIndex = (searchIndex, content) => {
+    let startIndex = searchIndex - 100;
 
-  const formatContent = (rawContent) => {
-    return rawContent
-      .replace(/([\.\?\!])[\n\s]{2,}/g, "$1 ")
-      .replace(/[\n\s]{2,}/g, ". ")
-      .replace(/\n/, " ");
-  }
+    if (startIndex < 0) { startIndex = 0; };
+
+    while (startIndex > 0 && content[startIndex] !== " ") {
+      startIndex = startIndex - 1;
+    };
+
+    if (content[startIndex] === " ") { startIndex++; };
+
+    return startIndex;
+  };
+
+  const getEndIndex = (searchIndex, queryLength, content) => {
+    let endIndex = searchIndex + queryLength + 100;
+
+    while (endIndex !== content.length && content[endIndex] !== " ") {
+      endIndex++;
+    };
+
+    return endIndex;
+  };
 
   const setHeading = (resultsCount, searchTerm) => {
     const searchHeading = document.getElementById("search-heading");
@@ -104,6 +102,8 @@
     searchHeading.innerText = `Showing ${resultsCount} ${resultsLabel} for "${searchTerm}"`
   };
 
+  const searchTerm = getQueryVariable("query");
+  
   if (searchTerm) {
     const searchInputs = Array.from(document.getElementsByClassName("search-form__input"));
 
@@ -136,5 +136,5 @@
     const results = index.search(searchTerm);
     displaySearchResults(results, window.store, searchTerm);
     setHeading(results.length, searchTerm);
-  }
+  };
 })();
