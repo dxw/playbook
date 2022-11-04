30d74343d679d3a2bb09309ed75d8e75f5f304c2
Ynda Jas <yndajas@gmail.com>
2022-11-04 12:26:08 +0000

    Add Standard package and fix errors
    This adds the Standard package and fixes linting errors in `search.js`
    
    Standard is the usual choice for dxw repos, although it might be
    permissible to use any opinionated linter/formatter
    
    @see https://github.com/dxw/tech-team-rfcs/blob/main/rfc-035-use-standard-linting-tools-across-all-our-projects.md
    
    Per the RFC linked above, we should consider whether to
    integrate this into CI flows and update the project README

--- src/assets/js/search.js
+++ src/assets/js/search.js
@@ -1,145 +1,146 @@
 (() => {
   const call = () => {
-    const searchQuery = getQueryVariable("query");
-    
+    const searchQuery = getQueryVariable('query')
+
     if (!searchQuery) { return }
 
-    document.getElementById("search-box").setAttribute("value", searchQuery);
+    document.getElementById('search-box').setAttribute('value', searchQuery)
 
-    const index = generateIndex();
-    const results = index.search(searchQuery);
+    const index = generateIndex()
+    const results = index.search(searchQuery)
 
-    displaySearchResults(results, searchQuery);
-    setHeading(results.length, searchQuery);
-  };
+    displaySearchResults(results, searchQuery)
+    setHeading(results.length, searchQuery)
+  }
 
   const getQueryVariable = (variable) => {
-    const urlQueryString = window.location.search;
-    const urlParams = new URLSearchParams(urlQueryString);
-    return urlParams.get(variable);
-  };
+    const urlQueryString = window.location.search
+    const urlParams = new URLSearchParams(urlQueryString)
+
+    return urlParams.get(variable)
+  }
 
   const generateIndex = () => {
     const index = elasticlunr(function () {
-      this.addField("title");
-      this.addField("content");
-      this.setRef("id");
+      this.addField('title')
+      this.addField('content')
+      this.setRef('id')
 
-      this.pipeline.remove(lunr.stemmer);
-    });
+      this.pipeline.remove(lunr.stemmer)
+    })
 
     const pages = Object.entries(window.store)
 
     pages.forEach(([pageKey, page]) => {
-      const content = formatContent(page.content);
-  
-      if (!content) { return };
-  
-      window.store[pageKey].content = content;
-  
+      const content = formatContent(page.content)
+
+      if (!content) { return }
+
+      window.store[pageKey].content = content
+
       index.addDoc({
         id: pageKey,
         title: page.title,
         content: page.content
-      });
+      })
     })
 
-    return index;
-  };
+    return index
+  }
 
   const formatContent = (rawContent) => {
     return rawContent
-      .replace(/([.?!])[\n\s]{2,}/g, "$1 ")
-      .replace(/[\n\s]{2,}/g, ". ")
-      .replace(/\n/, " ")
+      .replace(/([.?!])[\n\s]{2,}/g, '$1 ')
+      .replace(/[\n\s]{2,}/g, '. ')
+      .replace(/\n/, ' ')
       .trim()
-      .replace(/^.$/, '');
+      .replace(/^.$/, '')
   }
 
   const displaySearchResults = (results, searchQuery) => {
-    const searchResultsElement = document.getElementById("search-results");
+    const searchResultsElement = document.getElementById('search-results')
 
-    if (!results.length) { 
-      searchResultsElement.innerHTML = "<li class='search-results__no-results-message'>No results found.</li>";
-      return;
+    if (!results.length) {
+      searchResultsElement.innerHTML = '<li class="search-results__no-results-message">No results found.</li>'
+      return
     }
 
-    let innerHtml = "";
+    let innerHtml = ''
 
     results.forEach((result) => {
-      const item = window.store[result.ref];
+      const item = window.store[result.ref]
 
       const breadcrumbs = item.url
-        .replace(".html", "")
-        .replace(/-/g, " ")
-        .split("/")
+        .replace('.html', '')
+        .replace(/-/g, ' ')
+        .split('/')
         .filter(i => i)
         .map(breadcrumb => breadcrumb[0].toUpperCase() + breadcrumb.substring(1))
-      
-        breadcrumbs.pop();
 
-        const breadcrumbsString = breadcrumbs.join(' > ')
-        const excerpt = getExcerpt(item.content, searchQuery)
-  
-        innerHtml +=
-          '<li class="search-results__result">' +
-            `<a href="${item.url}">` +
-              `<h2 class="search-results__result-title">${item.title}</h2>` +
-            '</a>' +
-            (breadcrumbs.length ? `<div class="search-results__result-breadcrumbs">${breadcrumbsString}</div>` : '') +
-            `<p class="search-results__result-excerpt">...${excerpt}...</p>` +
-          '</li>';
-      });
-  
-    searchResultsElement.innerHTML = innerHtml;
-  };
+      breadcrumbs.pop()
+
+      const breadcrumbsString = breadcrumbs.join(' > ')
+      const excerpt = getExcerpt(item.content, searchQuery)
+
+      innerHtml +=
+        '<li class="search-results__result">' +
+          `<a href="${item.url}">` +
+            `<h2 class="search-results__result-title">${item.title}</h2>` +
+          '</a>' +
+          (breadcrumbs.length ? `<div class="search-results__result-breadcrumbs">${breadcrumbsString}</div>` : '') +
+          `<p class="search-results__result-excerpt">...${excerpt}...</p>` +
+        '</li>'
+    })
+
+    searchResultsElement.innerHTML = innerHtml
+  }
 
   const getExcerpt = (content, searchQuery) => {
-    const queryRegex = new RegExp(searchQuery, "i");
-    const matchIndex = content.search(queryRegex);
-    const queryLength = searchQuery.length;
+    const queryRegex = new RegExp(searchQuery, 'i')
+    const matchIndex = content.search(queryRegex)
+    const queryLength = searchQuery.length
     const excerpt = content.slice(
       getStartIndex(matchIndex, content),
-      getEndIndex(matchIndex, queryLength, content),
-    );
+      getEndIndex(matchIndex, queryLength, content)
+    )
 
     return excerpt.replace(
       queryRegex,
-      "<strong class='search-results__matching-keyword'>$&</strong>"
-    );
-  };
+      '<strong class="search-results__matching-keyword">$&</strong>'
+    )
+  }
 
   const getStartIndex = (matchIndex, content) => {
-    let startIndex = Math.max(matchIndex - 100, 0);
+    let startIndex = Math.max(matchIndex - 100, 0)
 
-    while (startIndex > 0 && content[startIndex] !== " ") {
-      startIndex = startIndex - 1;
-    };
+    while (startIndex > 0 && content[startIndex] !== ' ') {
+      startIndex = startIndex - 1
+    }
 
-    if (content[startIndex] === " ") { startIndex++; };
+    if (content[startIndex] === ' ') { startIndex++ }
 
-    return startIndex;
-  };
+    return startIndex
+  }
 
   const getEndIndex = (matchIndex, queryLength, content) => {
-    matchIndex = Math.max(matchIndex, 100);
+    matchIndex = Math.max(matchIndex, 100)
 
-    let endIndex = Math.min(matchIndex + queryLength + 100, content.length);
+    let endIndex = Math.min(matchIndex + queryLength + 100, content.length)
 
-    while (endIndex !== content.length && content[endIndex] !== " ") {
-      endIndex++;
-    };
+    while (endIndex !== content.length && content[endIndex] !== ' ') {
+      endIndex++
+    }
 
-    return endIndex;
-  };
+    return endIndex
+  }
 
   const setHeading = (resultsCount, searchQuery) => {
-    const searchHeading = document.getElementById("search-heading");
+    const searchHeading = document.getElementById('search-heading')
 
-    const resultsLabel = resultsCount == 1 ? "result" : "results";
+    const resultsLabel = resultsCount === 1 ? 'result' : 'results'
 
     searchHeading.innerText = `Showing ${resultsCount} ${resultsLabel} for "${searchQuery}"`
-  };
+  }
 
-  call();
-})();
+  call()
+})()
