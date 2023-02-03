bac765ce24c5bcb88bdb183d75701f96e391a3ff
Adam Hughes <adamhughes@urbanzoo.io>
2023-02-03 15:01:33 +0000

    Improve JS formatting

--- src/assets/js/redirect.js
+++ src/assets/js/redirect.js
@@ -1,12 +1,12 @@
-var fullPath = location.pathname + location.hash
+const fullPath = location.pathname + location.hash
 
 fetch('/redirects.json')
   .then((response) => response.json())
   .then((json) => {
-  var redirectArray = json.redirects
-  var redirectResult = redirectArray.find(item => item.from === fullPath);
+    const redirectArray = json.redirects
+    const redirectResult = redirectArray.find(item => item.from === fullPath)
 
-  if (redirectResult) {
-    location = redirectResult.to
-  }
-});
+    if (redirectResult) {
+      location = redirectResult.to
+    }
+})
